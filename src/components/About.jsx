542e344f9e94672bedcf45e81b8fed741d3ab13e
sarahdahippo <sarahcheah22@gmail.com>
2023-09-07 23:15:37 -0700

    Link art account

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,8 @@ const About = () => {
         As a user-focused developer with a passion for creative design, my long-term aspiration
         is to delve into entrepreneurship to improve how humans and machines interact physically and intellectually.
         <br/><br/>
-        In my free time, I love to bake, explore new places, do outdoor activities (hiking, kayaking) and art!
+        In my free time, I love to bake, explore new places, do outdoor activities (hiking, kayaking) and 
+        <a className={styles.link} href='https://www.instagram.com/eurekapalette/'>art</a>!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10 justify-center">
